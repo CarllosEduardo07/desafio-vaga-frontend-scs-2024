97c9780776c95c4e9d19a8b90ba867b686a8a80b
Carlos Eduardo - PC_Casa <eduardo201184@hotmail.com>
2024-09-12 00:39:38 -0300

    carrossel feito

--- src/components/cards-carro/cardCarro.tsx
+++ src/components/cards-carro/cardCarro.tsx
@@ -29,29 +29,31 @@ export default function CardCarro({
 }: CardCarroInterface) {
   return (
     <div>
-      <div className='flex flex-col  border-2 border-gray-400 rounded-3xl h-[350px] w-[250px] relative'>
-        <div className='px-8 space-y-5'>
-          <div className='mt-8 text-center font-normal text-xl'>
-            <img src={imagem} alt={modelo} width={250} height={250} className='absolute -ml-7 -top-24' />
-            <span>{modelo}</span>
-          </div>
-          <div className='flex items-center'>
-            <IconAno />
-            <span className='ml-2'>{ano}</span>
-          </div>
-          <div className='flex items-center'>
-            <IconKm />
-            <span className='ml-2'>{km}</span>
-          </div>
-          <div className='flex items-center'>
-            <IconPotencia />
-            <span className='ml-2'>{potencia}</span>
-          </div>
-          <div className='flex items-center'>
-            <IconPontuacao />
-            <span className='ml-2'>{pontuacao}</span>
-          </div>
+      <div className='px-8 space-y-5 flex flex-col border-2 border-gray-500/70 rounded-3xl h-[360px] w-[250px] relative'>
+        <div className=' relative mt-8 text-center font-normal text-xl'>
+          <img src={imagem} alt={modelo} width={250} height={250} className='-mt-11' />
+          <span>{modelo}</span>
         </div>
+        <div className='flex items-center'>
+          <IconAno />
+          <span className='ml-2'>{ano}</span>
+        </div>
+        <div className='flex items-center'>
+          <IconKm />
+          <span className='ml-2'>{km}</span>
+        </div>
+        <div className='flex items-center'>
+          <IconPotencia />
+          <span className='ml-2'>{potencia}</span>
+        </div>
+        <div className='flex items-center'>
+          <IconPontuacao />
+          <span className='ml-2'>{pontuacao}</span>
+        </div>
+
+        <button className='w-full px-4 py-2 text-center border-2 border-gray-500 rounded-full'>
+          <a href='#'>Ver Carro</a>
+        </button>
       </div>
     </div>
   );
--- src/components/carrossel/carrossel.tsx
+++ src/components/carrossel/carrossel.tsx
@@ -1,37 +1,48 @@
+import { CalendarDays, Gauge, Users, Zap } from 'lucide-react';
 import carros1 from '../../assets/cardsCarros/carro1.png';
 import carros2 from '../../assets/cardsCarros/carro2.png';
 import carros3 from '../../assets/cardsCarros/carro3.png';
 import carros4 from '../../assets/cardsCarros/carro4.png';
 import carros5 from '../../assets/cardsCarros/carro5.png';
 import carros6 from '../../assets/cardsCarros/carro6.png';
-import carros7 from '../../assets/cardsCarros/carro7.png';
-import carros8 from '../../assets/cardsCarros/carro8.png';
-import carros9 from '../../assets/cardsCarros/carro9.png';
-
 import CardCarro from '../cards-carro/cardCarro';
 
-import { CalendarDays, Gauge, Users, Zap } from 'lucide-react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import '../carrossel/styles.css';
+
+// Import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 export default function Carrossel() {
   return (
-    <div className='w-full h-screen flex space-x-20'>
-      <CardCarro imagem={carros1} modelo='Mustang' ano={2015} iconeAno={CalendarDays} km={'180 km/h'} iconeKm={Gauge} potencia={'9/10'} iconePotencia={Zap} pontuacao='8/10' iconePontuacao={Users} />
+    <div>
+      <Swiper modules={[Navigation, Pagination, Scrollbar]} spaceBetween={50} slidesPerView={3} navigation scrollbar={{ draggable: true }} className='mySwiper pb-10'>
+        <SwiperSlide>
+          <CardCarro imagem={carros1} modelo='Mustang' ano={2015} iconeAno={CalendarDays} km='180 km/h' iconeKm={Gauge} potencia='9/10' iconePotencia={Zap} pontuacao='8/10' iconePontuacao={Users} />
+        </SwiperSlide>
 
-      <CardCarro imagem={carros2} modelo='Mustang' ano={2015} iconeAno={CalendarDays} km={'180 km/h'} iconeKm={Gauge} potencia={'9/10'} iconePotencia={Zap} pontuacao='8/10' iconePontuacao={Users} />
+        <SwiperSlide>
+          <CardCarro imagem={carros2} modelo='Mustang' ano={2015} iconeAno={CalendarDays} km='180 km/h' iconeKm={Gauge} potencia='9/10' iconePotencia={Zap} pontuacao='8/10' iconePontuacao={Users} />
+        </SwiperSlide>
 
-      <CardCarro imagem={carros3} modelo='Mustang' ano={2015} iconeAno={CalendarDays} km={'180 km/h'} iconeKm={Gauge} potencia={'9/10'} iconePotencia={Zap} pontuacao='8/10' iconePontuacao={Users} />
-
-      {/* <CardCarro imagem={carros4} modelo='Mustang' ano={2015} iconeAno={CalendarDays} km={'180 km/h'} iconeKm={Gauge} potencia={'9/10'} iconePotencia={Zap} pontuacao='8/10' iconePontuacao={Users} />
-
-      <CardCarro imagem={carros5} modelo='Mustang' ano={2015} iconeAno={CalendarDays} km={'180 km/h'} iconeKm={Gauge} potencia={'9/10'} iconePotencia={Zap} pontuacao='8/10' iconePontuacao={Users} />
-
-      <CardCarro imagem={carros6} modelo='Mustang' ano={2015} iconeAno={CalendarDays} km={'180 km/h'} iconeKm={Gauge} potencia={'9/10'} iconePotencia={Zap} pontuacao='8/10' iconePontuacao={Users} />
-
-      <CardCarro imagem={carros7} modelo='Mustang' ano={2015} iconeAno={CalendarDays} km={'180 km/h'} iconeKm={Gauge} potencia={'9/10'} iconePotencia={Zap} pontuacao='8/10' iconePontuacao={Users} />
-
-      <CardCarro imagem={carros8} modelo='Mustang' ano={2015} iconeAno={CalendarDays} km={'180 km/h'} iconeKm={Gauge} potencia={'9/10'} iconePotencia={Zap} pontuacao='8/10' iconePontuacao={Users} />
-
-      <CardCarro imagem={carros9} modelo='Mustang' ano={2015} iconeAno={CalendarDays} km={'180 km/h'} iconeKm={Gauge} potencia={'9/10'} iconePotencia={Zap} pontuacao='8/10' iconePontuacao={Users} /> */}
+        <SwiperSlide>
+          <CardCarro imagem={carros3} modelo='Mustang' ano={2015} iconeAno={CalendarDays} km='180 km/h' iconeKm={Gauge} potencia='9/10' iconePotencia={Zap} pontuacao='8/10' iconePontuacao={Users} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CardCarro imagem={carros4} modelo='Mustang' ano={2015} iconeAno={CalendarDays} km='180 km/h' iconeKm={Gauge} potencia='9/10' iconePotencia={Zap} pontuacao='8/10' iconePontuacao={Users} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CardCarro imagem={carros5} modelo='Mustang' ano={2015} iconeAno={CalendarDays} km='180 km/h' iconeKm={Gauge} potencia='9/10' iconePotencia={Zap} pontuacao='8/10' iconePontuacao={Users} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CardCarro imagem={carros6} modelo='Mustang' ano={2015} iconeAno={CalendarDays} km='180 km/h' iconeKm={Gauge} potencia='9/10' iconePotencia={Zap} pontuacao='8/10' iconePontuacao={Users} />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
